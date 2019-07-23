0dc26637f68e16351b644913384b392283ae93fb
Damián Pumar <damianpumar@gmail.com>
2019-07-23 09:08:58 +0200

    Refactor tests

--- src/test/java/GameShould.java
+++ src/test/java/GameShould.java
@@ -1,19 +1,23 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameShould {
+    private Game game;
+
+    @Before
+    public void setup() {
+        this.game = new Game();
+    }
+
     @Test
     public void game_begins_with_0_survivors() {
-        Game game = new Game();
-
         assertThat(game.survivors().size()).isEqualTo(0);
     }
 
     @Test
     public void game_can_have_survivors() throws SurvivorDuplicatedException {
-        Game game = new Game();
-
         game.addSurvivor(new Survivor("Damián"));
 
         assertThat(game.survivors().size()).isEqualTo(1);
@@ -21,8 +25,6 @@ public class GameShould {
 
     @Test(expected = SurvivorDuplicatedException.class)
     public void game_can_have_unique_survivor_by_name() throws SurvivorDuplicatedException {
-        Game game = new Game();
-
         game.addSurvivor(new Survivor("Damián"));
 
         game.addSurvivor(new Survivor("Damián"));
@@ -31,8 +33,6 @@ public class GameShould {
     @Test
     public void game_ends_when_all_survivors_is_dead() throws SurvivorDuplicatedException {
         Survivor survivor = new Survivor("Damián");
-
-        Game game = new Game();
         game.addSurvivor(survivor);
         assertThat(game.finished()).isFalse();
 
