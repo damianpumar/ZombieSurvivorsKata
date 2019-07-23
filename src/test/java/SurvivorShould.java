a1040340beb8562079ff12fc92eaafd7b722d2d4
Damián Pumar <damianpumar@gmail.com>
2019-07-23 09:24:21 +0200

    Refactor tests

--- src/test/java/SurvivorShould.java
+++ src/test/java/SurvivorShould.java
@@ -1,26 +1,28 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SurvivorShould {
+    private Survivor survivor;
+
+    @Before
+    public void setup() {
+        this.survivor = new Survivor("Damián");
+    }
+
     @Test
     public void has_a_name() {
-        Survivor survivor = createSurvivor();
-
         assertThat(survivor.name()).isEqualTo("Damián");
     }
 
     @Test
     public void begins_zero_wounds() {
-        Survivor survivor = createSurvivor();
-
         assertThat(survivor.wounds()).isEqualTo(0);
     }
 
     @Test
     public void dies_immediately_when_receives_2_wounds() {
-        Survivor survivor = createSurvivor();
-
         survivor.receiveAttack();
         survivor.receiveAttack();
 
@@ -29,7 +31,6 @@ public class SurvivorShould {
 
     @Test
     public void additional_wounds_are_ignored() {
-        Survivor survivor = createSurvivor();
         survivor.receiveAttack();
         survivor.receiveAttack();
 
@@ -40,8 +41,6 @@ public class SurvivorShould {
 
     @Test
     public void first_2_pieces_of_equipment_carry_in_hand() {
-        Survivor survivor = createSurvivor();
-
         survivor.addEquipment("BaseBall bat");
         survivor.addEquipment("Frying pan");
 
@@ -50,7 +49,6 @@ public class SurvivorShould {
 
     @Test
     public void after_2_pieces_of_equipment_carry_in_reserve() {
-        Survivor survivor = createSurvivor();
         survivor.addEquipment("BaseBall bat");
         survivor.addEquipment("Frying pan");
 
@@ -63,7 +61,6 @@ public class SurvivorShould {
 
     @Test
     public void has_maximum_5_pieces_of_equipment() {
-        Survivor survivor = createSurvivor();
         survivor.addEquipment("BaseBall bat");
         survivor.addEquipment("Frying pan");
         survivor.addEquipment("Katana");
@@ -78,7 +75,6 @@ public class SurvivorShould {
 
     @Test
     public void reduces_pieces_of_equipment_carry_by_1_each_wound() {
-        Survivor survivor = createSurvivor();
         survivor.addEquipment("BaseBall bat");
         survivor.addEquipment("Frying pan");
         survivor.addEquipment("Katana");
@@ -93,19 +89,11 @@ public class SurvivorShould {
 
     @Test
     public void each_survivor_begins_with_0_experience() {
-        Survivor survivor = createSurvivor();
-
         assertThat(survivor.experience()).isEqualTo(0);
     }
 
     @Test
     public void each_survivor_begins_at_level_blue() {
-        Survivor survivor = createSurvivor();
-
         assertThat(survivor.level()).isEqualTo(LEVEL.blue);
-    }
-
-    private Survivor createSurvivor() {
-        return new Survivor("Damián");
     }
 }
